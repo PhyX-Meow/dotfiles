c5c7fbed581959ff3d30465f7707544cc2a69f8d
phyxmeow <XueHaotian@outlook.com>
2022-07-16 21:00:46 +0800

    20220716

--- alias.zsh
+++ alias.zsh
@@ -38,7 +38,7 @@ alias pluto="julia -e 'import Pluto;Pluto.run(port=8889)'"
 alias juhs="jupyter console --kernel=haskell"
 alias tlup='sudo tlmgr update --self --all --reinstall-forcibly-removed'
 alias tlcc='sudo rm /usr/local/texlive/current/tlpkg/backups/*'
-alias pyup='pip install --user --upgrade pip jupyterlab-mathjax3 jupyterlab_code_formatter jupyterlab-git theme-darcula jill wget'
+alias pyup='pip install --user --upgrade pip jupyterlab-mathjax3 jupyterlab-git theme-darcula jill wget'
 alias pycc='rm -rf ~/.cache/pip/http/* ~/.cache/pip/wheels/*'
 alias jlup="http_proxy=http://$win_host:8128 https_proxy=http://$win_host:8128 julia -e 'using Pkg;Pkg.update()'"
 alias jlcc="http_proxy=http://$win_host:8128 https_proxy=http://$win_host:8128 julia -e 'using Pkg,Dates;Pkg.gc(;collect_delay=Hour(0))'"
