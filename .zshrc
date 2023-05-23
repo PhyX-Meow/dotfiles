2b4492a017e48e01b525a1919877235b56e1d3d2
phyxmeow <XueHaotian@outlook.com>
2023-05-23 13:43:55 +0800

    Goodbye, jupyterlab.

--- .zshrc
+++ .zshrc
@@ -111,3 +111,20 @@ zinit light skywind3000/z.lua
 
 # asdf.sh
 . /opt/asdf-vm/asdf.sh
+
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba init' !!
+export MAMBA_EXE="/usr/bin/micromamba";
+export MAMBA_ROOT_PREFIX="/home/wuli/micromamba";
+__mamba_setup="$("$MAMBA_EXE" shell hook --shell zsh --prefix "$MAMBA_ROOT_PREFIX" 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__mamba_setup"
+else
+    if [ -f "/home/wuli/micromamba/etc/profile.d/micromamba.sh" ]; then
+        . "/home/wuli/micromamba/etc/profile.d/micromamba.sh"
+    else
+        export  PATH="/home/wuli/micromamba/bin:$PATH"  # extra space after export prevents interference from conda init
+    fi
+fi
+unset __mamba_setup
+# <<< mamba initialize <<<
--- alias.zsh
+++ alias.zsh
@@ -34,12 +34,12 @@ alias ls='exa --icons --group-directories-first'
 alias l='exa --long --icons --git --group-directories-first'
 alias ll='exa --long --all --icons --git --header --group-directories-first'
 alias clr='clear'
-alias lab='jupyter lab --no-browser'
+# alias lab='jupyter lab --no-browser'
 alias pluto="julia -e 'import Pluto;Pluto.run(port=8889)'"
-alias juhs="jupyter console --kernel=haskell"
+# alias juhs="jupyter console --kernel=haskell"
 alias tlup='sudo tlmgr update --self --all --reinstall-forcibly-removed'
 alias tlcc='sudo rm /opt/texlive/current/tlpkg/backups/*'
-alias pyup='pip install --user --upgrade pip theme-darcula'
+alias pyup='pip install --user --upgrade pip'
 alias pycc='rm -rf ~/.cache/pip/http/* ~/.cache/pip/wheels/*'
 alias jlup="http_proxy=http://$win_host:8128 https_proxy=http://$win_host:8128 julia -e 'using Pkg;Pkg.update()'"
 alias jlcc="http_proxy=http://$win_host:8128 https_proxy=http://$win_host:8128 julia -e 'using Pkg,Dates;Pkg.gc(;collect_delay=Hour(0))'"
